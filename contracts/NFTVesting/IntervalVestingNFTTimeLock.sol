f85253a464cd3470a851e1fd0f3a0083eeefa15e
ExtremelySunnyYk <chiayongkang@hotmail.com>
2022-07-17 11:04:57 +0800

    added enough eth check

--- contracts/NFTVesting/IntervalVestingNFTTimeLock.sol
+++ contracts/NFTVesting/IntervalVestingNFTTimeLock.sol
@@ -9,7 +9,7 @@ import "./IERC721.sol";
  * @dev A single NFT holder contract that will allow a beneficiary to extract the
  * NFT after a given release time with a discount sent to the beneficiary based on
  * the vesting duration of the NFT.
- * 
+ *
  * Note that in order for discount in ETH to be valid, ETH must first be sent to this contract upon token locking.
  */
 contract IntervalVestingNFTTimeLock {
@@ -37,7 +37,8 @@ contract IntervalVestingNFTTimeLock {
     // Discount for every interval passed
     uint256 private _discountPerInterval;
 
-
+    // Events
+    event EthReceived(address indexed sender, uint256 amount);
 
     modifier validRelease() {
         require(
@@ -50,10 +51,10 @@ contract IntervalVestingNFTTimeLock {
     /**
      * @dev Deploys a timelock instance that is able to hold the token specified, and will only release it to
      * `beneficiary_` when {release} is invoked after `releaseTime_`. The release time is specified as a Unix timestamp
-     * (in seconds). For every set of duration passed after the release time, the number of intervals would increase. 
-     *  The discount will then be applied to the beneficiary according to number of intervals the token has been vested for. 
-     * 
-     *  The developer would have to send ETH to this contract for discount to be applied after initialization
+     * (in seconds). For every set of duration passed after the release time, the number of intervals would increase.
+     *  The discount will then be applied to the beneficiary according to number of intervals the token has been vested for.
+     *
+     *  The developer would have to send ETH to this contract on contract deployement for discount to be applied.
      */
     constructor(
         IERC721 nft_,
@@ -70,8 +71,8 @@ contract IntervalVestingNFTTimeLock {
             "TimeLock: release time is before current time"
         );
         require(
-            0 < maxDiscount_ <= 100,
-            "TimeLock: max discount is greater than 100% or 0. Please use a valid maxDiscount."
+            maxDiscount_ <= 100,
+            "TimeLock: max discount is greater than 100%. Please use a valid maxDiscount."
         );
         require(
             discountPerInterval_ * maxInterval_ == maxDiscount_,
@@ -80,8 +81,7 @@ contract IntervalVestingNFTTimeLock {
         require(
             address(this).balance >= discountPerInterval_ * maxInterval_,
             "TimeLock: not enough ETH to pay for discount. Please send more ETH."
-        );        
-
+        );
 
         _nft = nft_;
         _tokenId = tokenId_;
@@ -124,10 +124,9 @@ contract IntervalVestingNFTTimeLock {
     /**
      * @dev Returns the max discount allowed for a token in percentage.
      */
-     function maxDiscount() public view virtual returns (uint256) {
+    function maxDiscount() public view virtual returns (uint256) {
         return _maxDiscount;
-     }
-
+    }
 
     /**
      * @dev Returns discount for locking at each Interval
@@ -164,7 +163,6 @@ contract IntervalVestingNFTTimeLock {
         return _maxIntervals - getCurrentInterval();
     }
 
-
     /**
      * @dev Get the current discount in terms of percentage for the vesting schedule.
      *
@@ -179,28 +177,38 @@ contract IntervalVestingNFTTimeLock {
     }
 
     /**
+     * @dev Returns discount accrued by the user up until the current interval.
+     */
+    function getDiscount() public view returns (uint256) {
+        return getDiscountPercentage() * address(this).balance;
+    }
+
+    /**
      * @dev Transfers NFT held by the timelock to the beneficiary. Will only succeed if invoked after the release
      * time. Sends the discount in Eth to the beneficiary.
      */
-
     function release() public virtual validRelease {
         require(
             nft().ownerOf(tokenId()) == address(this),
             "TimeLock: no NFT to release for user"
         );
-        require()
 
-        uint256 discount = getDiscount();
-        uint256 ethDiscount = discount * address(this).balance;
+        uint256 ethDiscount = getDiscount();
         (bool sent, ) = beneficiary().call{value: ethDiscount}("");
         require(sent, "Failed to send Ether");
 
         nft().safeTransferFrom(address(this), beneficiary(), tokenId());
     }
 
-    
     /**
-     * @dev Fallback function for eth to be sent to contract on Initialization.
+     * @dev Fallback function for eth to be sent to contract on Initialization. Emits EthReceived Event
      */
-    fallback() external payable{}
+    receive() external payable {
+        emit EthReceived(msg.sender, msg.value);
+    }
+
+    /**
+     * @dev Fallback function in the event that the contract is called directly.
+     */
+    fallback() external payable {}
 }
