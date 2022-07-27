061fc313e15116b822d3c6adde129cbc204dfe89
ExtremelySunnyYk <chiayongkang@hotmail.com>
2022-07-27 13:40:14 +0800

    formatting changes for basic NFT

--- contracts/NFTVesting/TestNFT/BasicNft.sol
+++ contracts/NFTVesting/TestNFT/BasicNft.sol
@@ -4,14 +4,15 @@ import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-
 contract BasicNft is ERC721 {
     string public constant TOKEN_URI =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
     uint256 private s_tokenCounter; // ID of NFT
 
-    constructor(string memory _name, string memory _tokenId) ERC721(_name, _tokenId) {
-        s_tokenCounter = 0; 
+    constructor(string memory _name, string memory _tokenId)
+        ERC721(_name, _tokenId)
+    {
+    s_tokenCounter = 0;
     }
 
     function mintNft() public returns (uint256) {
@@ -21,13 +22,20 @@ contract BasicNft is ERC721 {
     }
 
     // Gives us a JSON which contains the Image of the token.
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
         return TOKEN_URI;
     }
 
     function getTokenCounter() public view returns (uint256) {
         return s_tokenCounter;
     }
-
 }
