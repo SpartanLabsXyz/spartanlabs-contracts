360b39017ac46fcaade32ddca55cc458019b72cf
ExtremelySunnyYk <chiayongkang@hotmail.com>
2022-07-27 13:40:37 +0800

    updated deploy script for basic time lock

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-hardhat-local-deploy:
+hardhat-deploy-local-basic:
 	npx hardhat compile
-	npx hardhat run --network localhost scripts/NFTVesting/deploy.js
+	npx hardhat run --network localhost scripts/NFTVesting/deploy_basic_time_lock.js
 hardhat-chain:
 	npx hardhat node
--- scripts/NFTVesting/deploy_basic_time_lock.js
+++ scripts/NFTVesting/deploy_basic_time_lock.js
@@ -4,7 +4,10 @@ const { ethers } = require("hardhat");
 
 async function main() {
 	// Local Blockchain Deployment
-	const [owner, addr1] = await ethers.getSigners();
+	const [owner] = await ethers.getSigners();
+	console.log("Owner: ", owner.address);
+
+	console.log("\nDeploying NFT contract...");
 
 	// Deploying Basic NFT Contract
 	const basicNFTContract = await ethers.getContractFactory("BasicNft");
@@ -13,26 +16,30 @@ async function main() {
 	// Minting NFT
 	const basicMintTx = await basicNFTInstance.mintNft();
 	await basicMintTx.wait(1);
+
 	console.log(
 		`Basic NFT index 0 tokenURI: ${await basicNFTInstance.tokenURI(0)}`
 	);
 
 	const nftOwner = await basicNFTInstance.ownerOf(0);
-	console.log(`nftOwner: ${nftOwner.address}`);
+	console.log(`nftOwner: ${nftOwner}`);
 
 	// getting timestamp
 	const blockNumBefore = await ethers.provider.getBlockNumber();
 	const blockBefore = await ethers.provider.getBlock(blockNumBefore);
 	const timestampBefore = blockBefore.timestamp;
+	const timestampAfter = timestampBefore + 100;
 	console.log("timestampBefore: ", timestampBefore);
+
+	console.log("\nDeploying TimeLock contract...");
 
 	// Deploying Timelock
 	const timeLock = await ethers.getContractFactory("BasicNFTTimelock");
 	const timeLockInstance = await timeLock.deploy(
 		basicNFTInstance.address,
 		0,
-		addr1.address,
-		timestampBefore + 30
+		owner.address,
+		timestampAfter 
 	);
 
 	await timeLockInstance.deployed();
@@ -44,23 +51,46 @@ async function main() {
 	const nftLocked = await timeLockInstance.nft();
 	console.log("nftLocked: ", nftLocked);
 
-	// Set new timestamp
+	// Send NFT to timelock contract
+	await basicNFTInstance.transferFrom(
+		owner.address,
+		timeLockInstance.address,
+		0 // token id
+	);
+
+	// safe transfer from for only ERC721 Receiver implementer
+	// await basicNFTInstance["safeTransferFrom(address,address,uint256)"](
+	// 	owner.address,
+	// 	timeLockInstance.address,
+	// 	0
+	// );
+
+
+	// check owner of NFT after transfer to be timelock contract
+	const nftOwnerAfterTransfer = await basicNFTInstance.ownerOf(0);
+	console.log("nftOwnerAfterTransfer: ", nftOwnerAfterTransfer);
+
+	// Set new timestamp by speeding up time
 	await ethers.provider.send("evm_setNextBlockTimestamp", [
 		timestampBefore + 300,
 	]);
-	await ethers.provider.send("evm_mine"); // this one will have 02:00 PM as its timestamp
+	// await ethers.provider.send("evm_mine"); // this one will have 02:00 PM as its timestamp
 
 	// Get new timestamp
 	const blockNumAfter = await ethers.provider.getBlockNumber();
 	const blockAfter = await ethers.provider.getBlock(blockNumAfter);
-	const timestampAfter = blockAfter.timestamp;
-	console.log("timestampAfter: ", timestampAfter);
-
+	const currentTimeStamp = blockAfter.timestamp;
+	console.log("currentTimeStamp: ", currentTimeStamp, "\n");
 
 	// release NFT
+	console.log("Releasing NFT...");
 	const releaseTx = await timeLockInstance.release();
 	const newNftOwner = await basicNFTInstance.ownerOf(0);
-	console.log("newNftOwner: ", newNftOwner);
+	console.log("newNftOwner: ", newNftOwner); // same as original beneficiary
+
+	// // check if {release} function can be called again. Expected failure.
+	// const releaseTx2 = await timeLockInstance.release();
+	// console.log("releaseTx2: ", releaseTx2);
 }
 
 main()
