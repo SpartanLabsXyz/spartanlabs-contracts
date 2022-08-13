c6d5eda51ea5518c8416aaed19885d0579627c04
yongkang <chiayongkang@hotmail.com>
2022-08-13 10:20:51 +0800

    updated test

--- test/SoulBoundToken/test-BasicSBT.js
+++ test/SoulBoundToken/test-BasicSBT.js
@@ -6,7 +6,7 @@ const { ethers } = require('hardhat');
 describe('BasicSBT', function () {
 
   before(async () => {
-    [owner,user1,user2,user3] = await ethers.getSigners();
+    [owner, user1, user2, user3] = await ethers.getSigners();
     const SBTContract = await ethers.getContractFactory('BasicSBT');
     sbt = await SBTContract.deploy('Test SBT Token', 'SBT');
   });
@@ -22,7 +22,7 @@ describe('BasicSBT', function () {
 
   it('Should mint a new soul', async function () {
     const soul = ['SpartanLabs', 'https://spartanlabs.studio/'];
-    await sbt.mint(user1.address,soul);
+    await sbt.mint(user1.address, soul);
   });
 
   it('hasSoul should return true', async function () {
@@ -38,17 +38,29 @@ describe('BasicSBT', function () {
 
   it('validateAttribute should return true', async function () {
     const expectedSoul = ['SpartanLabs', 'https://spartanlabs.studio/'];
-    expect(await sbt.validateAttribute(user1.address,expectedSoul)).to.equal(true);
+    expect(await sbt.validateAttribute(user1.address, expectedSoul)).to.equal(true);
   });
 
   it('validateAttribute should return false', async function () {
     const expectedSoul = ['SpartanLabss1', 'https://spartanlabs.studio/'];
-    expect(await sbt.validateAttribute(user1.address,expectedSoul)).to.equal(false);
+    expect(await sbt.validateAttribute(user1.address, expectedSoul)).to.equal(false);
   });
 
-  it('User should be able to update soul', async function () {
+  it('User should not be able to update soul', async function () {
     const soul = ['Spartan', 'https://www.spartangroup.io/team.html'];
-    await sbt.updateSBT(user1.address,soul);
+    await expect(sbt.connect(user1).updateSBT(user1.address, soul)).to.be.revertedWith('Ownable: caller is not the owner');
+  });
+
+  it('getSoul should return the correct identifier after User attempted update', async function () {
+    const soul = await sbt.getSBTData(user1.address);
+    //console.log(soul);
+    expect(soul[0]).to.equal('SpartanLabs');
+    expect(soul[1]).to.equal('https://spartanlabs.studio/');
+  });
+
+  it('Owner should be able to update soul', async function () {
+    const soul = ['Spartan', 'https://www.spartangroup.io/team.html'];
+    await sbt.updateSBT(user1.address, soul);
   });
 
   it('getSoul should return the updated value', async function () {
@@ -60,11 +72,17 @@ describe('BasicSBT', function () {
 
   it('validateAttribute should return true after update', async function () {
     const expectedSoul = ['Spartan', 'https://www.spartangroup.io/team.html'];
-    expect(await sbt.validateAttribute(user1.address,expectedSoul)).to.equal(true);
+    expect(await sbt.validateAttribute(user1.address, expectedSoul)).to.equal(true);
   });
 
-  it('User should be able to delete their data', async function () {
-    await sbt.connect(user1).burn(user1.address);
+  it('User should not be able to delete their data', async function () {
+    await expect(sbt.connect(user1).burn(user1.address)).to.be.revertedWith('Ownable: caller is not the owner');
+    expect(await sbt.hasSoul(user1.address)).to.equal(true);
+
+  });
+
+  it('Owner should be able to delete their data', async function () {
+    await sbt.connect(owner).burn(user1.address);
   });
 
   it('hasSoul should return false after delete', async function () {
@@ -73,7 +91,7 @@ describe('BasicSBT', function () {
 
   it('Should mint another soul for user2', async function () {
     const soul = ['Alice Smith', 'https://github.com'];
-    await sbt.mint(user2.address,soul);
+    await sbt.mint(user2.address, soul);
     // check that user2 has a soul
     expect(await sbt.hasSoul(user2.address)).to.equal(true);
   });
